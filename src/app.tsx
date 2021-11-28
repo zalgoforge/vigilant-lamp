68aef0d348d17c6329acbd561182c6802e952e90
Krzysztof Kabat <odrinwhite@gmail.com>
2021-11-28 22:08:41 +0000

    current state

--- src/app.tsx
+++ src/app.tsx
@@ -1,43 +1,98 @@
-import React, { Suspense } from "react";
-import {
-  Box,
-  Html,
-  softShadows,
-  useProgress,
-} from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { Knight } from "./components/knight";
+import React, { useEffect, useMemo, useRef } from "react";
+import { OrbitControls, softShadows, Box, RoundedBox } from "@react-three/drei";
+import { Canvas, useThree } from "@react-three/fiber";
+import { Material, Mesh, Vector2, Raycaster, MeshPhongMaterial } from "three";
 
 softShadows();
 
-function Loader() {
-  const { progress } = useProgress();
-  return <Html center>{progress.toFixed(0)} % loaded</Html>;
-}
-
-export const App = () => (
-  <Canvas shadows={true}>
-    <ambientLight intensity={0.5} />
-    <directionalLight
-      position={[0, 10, 0]}
-      intensity={1.5}
-      castShadow={true}
-      shadow-mapSize-height={1024}
-      shadow-mapSize-width={1024}
-      shadow-camera-far={50}
-      shadow-camera-left={-10}
-      shadow-camera-right={10}
-      shadow-camera-top={10}
-      shadow-camera-bottom={-10}
-    />
-    <Suspense fallback={<Loader />}>
-      <Knight />
-    </Suspense>
-    <Box position={[0, 1, 0]} />
-    <Box position={[-2, 1, 2]} />
-    <mesh receiveShadow={true} rotation={[-Math.PI / 2, 0, 0]}>
-      <planeBufferGeometry attach="geometry" args={[100, 100]} />
-      <shadowMaterial attach="material" opacity={0.3} />
-    </mesh>
-  </Canvas>
+const state = Array.from({ length: 10 }, () =>
+  Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => 0))
 );
+
+const Board = () => {
+  const { camera } = useThree();
+  const raycaster = useMemo(() => new Raycaster(), []);
+  const boardRef = useRef<THREE.Object3D>();
+
+  useEffect(() => {
+    const mouse = new Vector2();
+    const onMouseMove = (e: MouseEvent) => {
+      mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+      mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+    };
+    const onClick = (e: MouseEvent) => {
+      raycaster.setFromCamera(mouse, camera);
+      const intersects = raycaster.intersectObjects(
+        boardRef.current?.children || []
+      );
+      if (intersects.length) {
+        const [target] = intersects;
+        if (target.object) {
+          const material = (target.object as Mesh)
+            .material as MeshPhongMaterial;
+          material.color.set(0x00ff00);
+        }
+      }
+    };
+
+    window.addEventListener("mousemove", onMouseMove);
+    window.addEventListener("click", onClick);
+    return () => {
+      window.removeEventListener("mousemove", onMouseMove);
+      window.removeEventListener("click", onClick);
+    };
+  }, []);
+
+  return (
+    <group ref={boardRef}>
+      {state.map((row, x) =>
+        row.map((col, y) =>
+          col.map((_, z) => (
+            <group key={`${x}-${y}-${z}`}>
+              <RoundedBox
+                args={[1, 1, 1]}
+                radius={0.05}
+                smoothness={4}
+                position={[x, y, z]}
+              >
+                <meshPhongMaterial attach="material" color="#f3f3f3" />
+              </RoundedBox>
+            </group>
+          ))
+        )
+      )}
+    </group>
+  );
+};
+
+export const App = () => {
+  return (
+    <Canvas shadows={true}>
+      <ambientLight intensity={0.5} />
+      <directionalLight
+        position={[0, 10, 0]}
+        intensity={1.5}
+        castShadow={true}
+        shadow-mapSize-height={1024}
+        shadow-mapSize-width={1024}
+        shadow-camera-far={50}
+        shadow-camera-left={-10}
+        shadow-camera-right={10}
+        shadow-camera-top={10}
+        shadow-camera-bottom={-10}
+      />
+      <Board />
+      <group position={[-0.5, -0.5, -0.5]}>
+        <Box args={[10, 0.1, 10]} position={[0, 0, 0]}>
+          <meshPhongMaterial
+            attach="material"
+            color="#f30000"
+            opacity={0.4}
+            transparent={true}
+          />
+        </Box>
+      </group>
+      <OrbitControls />
+    </Canvas>
+  );
+};
